fbe4df88da6ab2a49823123725ab478c6b4d99cf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-13 13:20:59 -0800

    add StyleBox

--- src/components/BoxDisplay.jsx
+++ src/components/BoxDisplay.jsx
@@ -1,4 +1,5 @@
 import react, { useState } from "react";
+import StyledBox from "./StyledBox";
 
 ////////////////////////////////////////////////////////////////
 //  BOX DISPLAY COMPONENT
@@ -8,8 +9,12 @@ const BoxDisplay = (props) => {
     return (
         <div>
             <h1>In Display Box Component</h1>
-            {props.colors.map((color, i) =>
-                    )}
+            
+            <div className="row">
+                {props.colors.map((color, i) =>
+                    <div className="col m-3"><StyledBox bgColor={color}/></div>)}
+            </div>
+            
         </div>
     );
 }
