e69fab381b6867062f202c23bc0b3218cfa25fbf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-13 11:16:37 -0800

    add setNewColor in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
 import ColorForm from './components/ColorForm';
 import BoxDisplay from './components/BoxDisplay';
+import { useState } from 'react';
 
 //////////////////////////////////////////////////////////////////
 //  ROOT APPLICATION
 //////////////////////////////////////////////////////////////////
 
 function App() {
+
+  const [colors, setColors] = useState([]);
+
+  const setNewColor = (newColor) => {
+    setColors( colors.map(color => color ).push(newColor) );
+  }
+
   return (
     <div className="App">
       {/* //// COLOR FORM COMPONENT ////////////////////////////// */}
       <div className="bg-info rounded mb-3 p-2">
-        <ColorForm />
+        <ColorForm onNewColor={ setNewColor }/>
       </div>
 
       {/* //// DISPLAY BOC COMPONENT ///////////////////////////// */}
       <div className="border rounded mb-3 p-2">
-        <BoxDisplay />
+        <BoxDisplay colors={ colors }/>
+        {
+          console.log("**** In Display in App.js******");
+          console.log("Colors:",colors);
+        }
       </div>
     </div>
   );
--- src/components/ColorForm.jsx
+++ src/components/ColorForm.jsx
@@ -10,6 +10,11 @@ const ColorForm = (props) => {
             <h1>In Color Form Component</h1>
         </div>
     );
+
+    const handleColor = (e) => {
+        e.preventDefault();
+        props.setNewColor( e.target.value )
+    }
 }
 
 export default ColorForm;
