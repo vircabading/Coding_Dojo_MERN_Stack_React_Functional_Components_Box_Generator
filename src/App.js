b00a27c45d2a5530696b283ec94887b16a233124
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-13 06:11:13 -0800

    layout ColorForm and DisplayBox Component locations in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* //// COLOR FORM COMPONENT ////////////////////////////// */}
+      <div className="bg-info rounded mb-3 p-2">
+        <h1>Form Component</h1>
+      </div>
 
+      {/* //// DISPLAY BOC COMPONENT ///////////////////////////// */}
+      <div className="border rounded mb-3 p-2">
+        <h1>Display Box Component</h1>
+      </div>
     </div>
   );
 }
